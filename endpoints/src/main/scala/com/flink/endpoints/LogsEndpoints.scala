d23174d89f07aae5c158419cd8a806619fb2de97
gnoorts <noorts@yahoo.com>
2021-10-27 10:57:18 -0500

    Add endpoints project

---
+++ endpoints/src/main/scala/com/flink/endpoints/LogsEndpoints.scala
@@ -0,0 +1,16 @@
+package com.flink.endpoints
+
+import org.json4s.ext.JavaTypesSerializers
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.ScalatraServlet
+
+
+class MyScalatraServlet extends ScalatraServlet {
+
+  protected implicit lazy val jsonFormats: Formats = DefaultFormats ++ JavaTypesSerializers.all
+
+  get("/") {
+    "hi"
+  }
+
+}
